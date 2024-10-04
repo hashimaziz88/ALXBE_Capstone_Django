fc29b351c0cf33fb8ea98ce4eef6eb276ee606bc
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-10-04 17:56:53 +0200

    added  allowed_hosts

--- movie_review_api/movie_review_api/settings.py
+++ movie_review_api/movie_review_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)v(m(d#zm^9%4o%m9sa1t!zln1!ynh-&)82@ulc5yb%(@6_3kq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["hashimaziz88.pythonanywhere.com"]
 
 
 INSTALLED_APPS = [
