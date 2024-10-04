d63be21035fff1489595fcf19eabb1f36b3fe2ca
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-10-04 17:43:35 +0200

    update settings.py

--- movie_review_api/movie_review_api/settings.py
+++ movie_review_api/movie_review_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)v(m(d#zm^9%4o%m9sa1t!zln1!ynh-&)82@ulc5yb%(@6_3kq"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
